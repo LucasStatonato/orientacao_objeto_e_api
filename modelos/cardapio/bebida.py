3591948347b2e18e14df04356fc4f0e5fa5b4fdb
lucasstatonato <statonatolucas@gmail.com>
2025-02-18 21:43:28 -0300

    Orientação ao objeto aula 1

--- modelos/cardapio/bebida.py
+++ modelos/cardapio/bebida.py
@@ -1,3 +1,10 @@
-class Bebida:
+from modelos.cardapio.item_cardapio import ItemCardpio
+
+
+class Bebida(ItemCardpio):
     def __init__(self, nome, preco, tamanho):
-        pass
+        super().__init__(nome, preco)
+        self.tamanho = tamanho
+
+    def __str__(self):
+        return self._nome
--- modelos/cardapio/prato.py
+++ modelos/cardapio/prato.py
@@ -1,3 +1,10 @@
-class Prato:
+from modelos.cardapio.item_cardapio import ItemCardpio
+
+
+class Prato(ItemCardpio):
     def __init__(self, nome, preco, descricao):
-        pass
+        super().__init__(nome, preco)
+        self.descricao = descricao
+
+    def __str__(self):
+        return self._nome
